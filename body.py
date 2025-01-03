06932e93b6b1b36a8f0308d712bff14334a28160
Stefio Yosse Andrean <yosseandrean@gmail.com>
2025-01-03 16:22:33 +0100

    accelaration is moved to CelestialBody

--- body.py
+++ body.py
@@ -36,6 +36,20 @@ class CelestialBody(turtle.Turtle):
         self.setx(self.xcor() + self.velocity[0])
         self.sety(self.ycor() + self.velocity[1])
 
+    def accelerate_due_to_gravity(self, body) -> None:
+        force = self.mass * body.mass / self.distance(body) ** 2
+        angle = self.towards(body)
+        acceleration = force / self.mass
+
+        acc_x = acceleration * math.cos(math.radians(angle))
+        acc_y = acceleration * math.sin(math.radians(angle))
+
+        # Update the velocity
+        self.velocity = (
+            self.velocity[0] + acc_x,
+            self.velocity[1] + acc_y,
+        )
+
 
 class SolarSystem:
 
@@ -58,24 +72,9 @@ class SolarSystem:
             body.draw()
         self.solar_system.update()
 
-    # TODO: move this to the CelestialBody
-    @staticmethod
-    def accelerate_due_to_gravity(first: CelestialBody, second: CelestialBody):
-        force = first.mass * second.mass / first.distance(second) ** 2
-        angle = first.towards(second)
-        reverse = 1
-        for body in first, second:
-            acceleration = force / body.mass
-            acc_x = acceleration * math.cos(math.radians(angle))
-            acc_y = acceleration * math.sin(math.radians(angle))
-            body.velocity = (
-                body.velocity[0] + (reverse * acc_x),
-                body.velocity[1] + (reverse * acc_y),
-            )
-            reverse = -1
-
     def calculate_all_body_interactions(self):
-        bodies_copy = self.bodies.copy()
-        for idx, first in enumerate(bodies_copy):
-            for second in bodies_copy[idx + 1 :]:
-                self.accelerate_due_to_gravity(first, second)
+        for first in self.bodies:
+            for second in self.bodies:
+                if first is second:
+                    continue
+                first.accelerate_due_to_gravity(second)
